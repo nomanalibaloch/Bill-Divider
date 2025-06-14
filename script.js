fd4777af8dc4c9ec2c456737b1bdea96c09c175c
nomanalibaloch <121455819+nomanalibaloch@users.noreply.github.com>
2025-06-14 19:41:59 +0500

    Update script.js

--- script.js
+++ script.js
@@ -6,10 +6,11 @@ const generateBill=document.querySelector('.generate-bill');
 const tipAmount=document.querySelector('.amount');
 const totalAmount=document.querySelector('.total-amount');
 const eachPersonAmount=document.querySelector('.each-person');
-const reset=document.querySelector('.reset');
+const reset=document.querySelector('.reset-btn');
 let numOfPeople;
 let totalBill;
 let discount;
+let discountApplied=false;
 function removeButtonClass(){
         tipBtns.forEach((z)=>{
         z.classList.remove('selected');
@@ -33,31 +34,39 @@ billAmount.addEventListener('change',(e)=>{
     removeclass(customTip,'not-allowed');
     removeclass(people,'not-allowed');
 })
-
 tipBtns.forEach((x)=>{
-    x.addEventListener('click',(totalBill)=>{
+    x.addEventListener('click',()=>{
         removeButtonClass();
         addButtonClass(x);
         discount=parseInt(x.getAttribute('discount'));
+        customTip.value='';
         discountApplied=true;
         if(numOfPeople){
         removeclass(generateBill,'not-allowed');
         removeAttribute(generateBill,'disabled');
         }
+    
     })
 })
-customTip.addEventListener('input',(e)=>{
+customTip.addEventListener('input', (e) => { 
     removeButtonClass();
-    discount=+e.target.value;
-    discountApplied=true;
-    if(numOfPeople){
-        removeclass(generateBill,'not-allowed');
-        removeAttribute(generateBill,'disabled');
+    discount = +e.target.value;
+    
+    
+    if (e.target.value === "" || isNaN(discount)) {
+        discountApplied = false;
+    } else {
+        discountApplied = true; 
     }
-})
+
+    if (discountApplied && numOfPeople) {
+        removeclass(generateBill, 'not-allowed');
+        removeAttribute(generateBill, 'disabled');
+    }
+});
 people.addEventListener('input',(e)=>{
     numOfPeople=+e.target.value;
-    if(discount){
+    if(discountApplied){
         removeclass(generateBill,'not-allowed');
         removeAttribute(generateBill,'disabled');
     }
